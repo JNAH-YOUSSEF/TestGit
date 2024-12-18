970966ffba14fe1b131651d770c21bd56ba1c599
JANH YOUSSEF <151456292+JNAH-YOUSSEF@users.noreply.github.com>
2024-12-18 21:30:44 +0100

    add reset button and change something style and then change add to increment in button

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let span = document.getElementById('span')
 let count = 0
 
 function Add() {
-    count++
+    count += 1 
     span.innerHTML = count
+}
+
+
+function Reset() {
+    count = 0 
+    span.innerHTML = 0 
 }
