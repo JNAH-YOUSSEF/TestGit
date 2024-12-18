34bf04862e4cb08e89b4ca7cd9078d05cb6680b9
JANH YOUSSEF <151456292+JNAH-YOUSSEF@users.noreply.github.com>
2024-12-18 21:36:12 +0100

    add feauter input onchange

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ function Reset() {
     count = 0 
     span.innerHTML = 0 
 }
+
+
+
+let input = document.getElementById('text')
+
+input.addEventListener('change', (e) => {
+    
+    e.target.nextElementSibling.innerHTML = e.target.value
+})
